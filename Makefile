44eb11dc15c0595d037c4200debf6a6935964a3d
Bubbq <soccer17nemo@gmail.com>
2024-03-25 17:46:56 -0500

    basic movement with projectiles

--- Makefile
+++ Makefile
@@ -1,6 +1,8 @@
-all:
+animate:
 	g++ animation.cpp -o run -lraylib
 
+edit:
+	g++ editor.cpp -o run -lraylib
 clean:
 	rm -r ./levels/level*
 	rm run	
--- animation.cpp
+++ animation.cpp
@@ -1,3 +1,4 @@
+#include <cmath>
 #include <raylib.h>
 #include <string>
 #include <vector>
@@ -8,6 +9,8 @@ const std::string SKELETON_PATH = "assets/Character_animation/monsters_idle/skel
 const std::string VAMPIRE_PATH = "assets/Character_animation/monsters_idle/vampire/v2/vampire_v2_";
 const std::string PREIST_PATH = "assets/Character_animation/priests_idle/priest1/v1/priest1_v1_";
 const std::string SKULL_PATH = "assets/Character_animation/monsters_idle/skull/v2/skull_v2_";
+const std::string ARROW_PATH = "assets/arrow_4.png";
+const std::string BOW_PATH = "assets/bow.png";
 
 Texture2D skeletonTexture[4];
 Texture2D vampireTexture[4];
@@ -18,6 +21,8 @@ struct Sprite{
     std::string name;
     Vector2 pos;
     int fc;
+    float sx;
+    float sy;
 };
 
 std::vector<Sprite> worldSprites;
@@ -53,8 +58,8 @@ void initAnimation(){
     }
 }
 
-// to animate a sprite
-void animate(){
+// to animate world sprites
+void animate(Texture2D ARROW){
     // inc the frame count fo every sprite, then based on its name, find the appropriate textture to draw
     for(int i = 0; i < (int)worldSprites.size(); i++){
         worldSprites[i].fc++;
@@ -63,37 +68,81 @@ void animate(){
             fp = 0;
             worldSprites[i].fc = 0;
         }
+
         // based on the sprites name, draw the approprate frame
         if(worldSprites[i].name == "skeleton")
-            DrawTexturePro(skeletonTexture[fp], {0,0,16,16}, {worldSprites[i].pos.x, worldSprites[i].pos.x, 16 * 2, 16 * 2}, {0,0}, 0, WHITE);
+            DrawTexturePro(skeletonTexture[fp], {0,0,16,16}, {worldSprites[i].pos.x, worldSprites[i].pos.y, 16 * 2, 16 * 2}, {0,0}, 0, WHITE);
         else if(worldSprites[i].name == "vampire")
-            DrawTexturePro(vampireTexture[fp], {0,0,16,16}, {worldSprites[i].pos.x, worldSprites[i].pos.x, 16 * 2, 16 * 2}, {0,0}, 0, WHITE);
+            DrawTexturePro(vampireTexture[fp], {0,0,16,16}, {worldSprites[i].pos.x, worldSprites[i].pos.y, 16 * 2, 16 * 2}, {0,0}, 0, WHITE);
         else if(worldSprites[i].name == "priest")
-            DrawTexturePro(preistTexture[fp], {0,0,16,16}, {worldSprites[i].pos.x, worldSprites[i].pos.x, 16 * 2, 16 * 2}, {0,0}, 0, WHITE);
-        else 
-            DrawTexturePro(skullTexture[fp], {0,0,16,16}, {worldSprites[i].pos.x, worldSprites[i].pos.x, 16 * 2, 16 * 2}, {0,0}, 0, WHITE);
+            DrawTexturePro(preistTexture[fp], {0,0,16,16}, {worldSprites[i].pos.x, worldSprites[i].pos.y, 16 * 2, 16 * 2}, {0,0}, 0, WHITE);
+        else if(worldSprites[i].name == "skull") 
+            DrawTexturePro(skullTexture[fp], {0,0,16,16}, {worldSprites[i].pos.x, worldSprites[i].pos.y, 16 * 2, 16 * 2}, {0,0}, 0, WHITE);
+        else if(worldSprites[i].name == "arrow")
+            DrawTexturePro(ARROW, {0,8,16,16}, {worldSprites[i].pos.x, worldSprites[i].pos.y, 32, 32}, {0,0}, 0, WHITE);
+
+        // update sprites movement  
+        if(worldSprites[i].sx != 0 && worldSprites[i].sy != 0){
+            worldSprites[i].pos.x +=  7 * worldSprites[i].sx;
+            worldSprites[i].pos.y += 7 * worldSprites[i].sy;
+        }
     } 
 }
 
+void drawPlayer(Sprite& player, Texture2D BOW, Texture2D ARROW){
+    if(IsKeyDown(KEY_W))
+        player.pos.y -= 5;
+    if(IsKeyDown(KEY_A))
+        player.pos.x -= 5;
+    if(IsKeyDown(KEY_S))
+        player.pos.y += 5;
+    if(IsKeyDown(KEY_D))
+        player.pos.x += 5;
+
+    // collisions
+
+
+
+    float x = GetMouseX() - player.pos.x;
+    float y = GetMouseY() - player.pos.y;
+    float rotation = atan2(x, y) * -57.29578f;
+
+    // drawing the users bow
+    DrawTexturePro(BOW, {0,0,16,16}, {float(player.pos.x + cos((rotation + 45) * DEG2RAD) * 32) + 16, float(player.pos.y + sin((rotation + 45) * DEG2RAD) * 32) + 16, 32, 32}, {0,0}, rotation + 135, WHITE);
+
+    // when user shoots bow
+    if(IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
+        worldSprites.push_back({"arrow", {player.pos.x + 16, player.pos.y + 16}, 0, float(cos((rotation + 90) * DEG2RAD)), float(sin((rotation + 90) * DEG2RAD))}); 
+}
+
 int main(){
+
+    // TODO make a projectile strut to handle the arrows and other future projectiles, literally only going to have arrows
+    // handle collisions
+
     SetTraceLogLevel(LOG_ERROR);
-    InitWindow(500, 500, "animation");
+    InitWindow(750,750, "animation");
     SetTargetFPS(60);
     initAnimation();
-    int offset = 50;
-      
-    for(int i = 0; i < 5; i++){
-        worldSprites.push_back({"priest", {float(offset + GetScreenWidth() * 0.5), float(GetScreenHeight() * 0.5)}, 0});
-        offset += 50;
-    }
-   
+    Texture2D BOW = LoadTextureFromImage(LoadImage(BOW_PATH.c_str()));
+    Texture2D ARROW = LoadTextureFromImage(LoadImage(ARROW_PATH.c_str()));
+    Sprite player;
+    player.name = "priest";
+    player.pos = {float(GetScreenWidth() * 0.5), float(GetScreenHeight() * 0.5)};
+    player.fc = 0;
+    player.sx = 0;
+    worldSprites.push_back(player);
+
     while (!WindowShouldClose()) {
         BeginDrawing();
         ClearBackground(BLACK);
-        animate();
+        drawPlayer(worldSprites[0], BOW, ARROW);
+        animate(ARROW);
         EndDrawing();
     }
 
+    UnloadTexture(ARROW);
+    UnloadTexture(BOW);
     unloadTextures();
     CloseWindow();
 }
